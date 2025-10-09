c3efeb896995f76c849c496731663ad6d6ebaf05
esblinov <esblinov@avito.ru>
2025-10-09 16:00:41 +0300

    Add unit test for LockTraceWrapper basic tracing

--- tests/units/locks/tracer/test_tracer.py
+++ tests/units/locks/tracer/test_tracer.py
@@ -1,1 +1,21 @@
-from locklib import LockTraceWrapper
+from typing import List, Union
+from locklib import LockTraceWrapper, TracerEvent
+
+
+def test_base_trace_with_methods():
+    class PseudoLock:
+        def acquire(self):
+            self.trace.append('acquire')
+        def release(self):
+            self.trace.append('release')
+        def set_trace_collection(self, trace: List[Union[str, TracerEvent]]):
+            self.trace = trace
+
+    pseudo_lock = PseudoLock()
+    wrapper = LockTraceWrapper(pseudo_lock)
+    pseudo_lock.set_trace_collection(wrapper.trace)
+
+    wrapper.acquire()
+    wrapper.release()
+
+    assert wrapper.trace == ['acquire', TracerEvent.ACQUIRE, 'release', TracerEvent.RELEASE]
