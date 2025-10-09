e4f84cdfaaf76717e2559ce76fbb25fdfef27c25
esblinov <esblinov@avito.ru>
2025-10-09 16:00:53 +0300

    Add unit test for LockTraceWrapper context manager

--- tests/units/locks/tracer/test_tracer.py
+++ tests/units/locks/tracer/test_tracer.py
@@ -19,3 +19,22 @@ def test_base_trace_with_methods():
     wrapper.release()
 
     assert wrapper.trace == ['acquire', TracerEvent.ACQUIRE, 'release', TracerEvent.RELEASE]
+
+
+def test_base_trace_with_context_manager():
+    class PseudoLock:
+        def acquire(self):
+            self.trace.append('acquire')
+        def release(self):
+            self.trace.append('release')
+        def set_trace_collection(self, trace: List[Union[str, TracerEvent]]):
+            self.trace = trace
+
+    pseudo_lock = PseudoLock()
+    wrapper = LockTraceWrapper(pseudo_lock)
+    pseudo_lock.set_trace_collection(wrapper.trace)
+
+    with wrapper:
+        pass
+
+    assert wrapper.trace == ['acquire', TracerEvent.ACQUIRE, 'release', TracerEvent.RELEASE]
